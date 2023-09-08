fb94b50c3c8990ce4ab323206688b4d32a3d713f
Larysa Semerikova <lorik280319@gmail.com>
2023-09-08 16:49:45 +0300

    add more js

--- scripts/work.js
+++ scripts/work.js
@@ -1,43 +1,65 @@
-const tabs = document.querySelector(".work__tabs")
-const blocks = Array.from(document.querySelectorAll(".work__box"))
+function showImg() {
+  const images = document.querySelectorAll(`.work__img`);
 
-let activeCards = blocks.slice(0, 12)
+  images.forEach((image) => {
+    const workBlock = document.querySelector(".work__block");
+    const elem = document.createElement("div");
+    elem.classList = "work__box";
+    elem.innerHTML += `
+      <div class="work__img active__img">
+        <img
+          src="./images/our__amazing__work/icon.png"
+          alt="work__img"
+        />
+        <span>creative design</span>
+        <span>Web Design</span>
+      </div>
+      <div class="work__img">
+        <img
+          src="${image.src}"
+          alt="work__img"
+        />
+      </div>`;
+    workBlock.append(elem);
+  });
+}
 
-let unActiveCards = blocks.slice(12, 24)
+showImg();
 
-unActiveCards.forEach(card =>{
-    card.style.display = "none"
-})
+const tabs = document.querySelector(".work__tabs");
+const blocks = Array.from(document.querySelectorAll(".work__box"));
 
-tabs.addEventListener("click", showPhotos)
-  
-function showPhotos(event){ 
-    let target = event.target.dataset.tab 
+let activeCards = blocks.slice(0, 12);
 
-    if(event.target.tagName !== "LI") return false;
+let unActiveCards = blocks.slice(12, 24);
 
-    activeCards.forEach(item =>{
-    if (item.classList.contains(target) || target === "all"){
-        item.style.display = "block"
+unActiveCards.forEach((card) => {
+  card.style.display = "none";
+});
 
+tabs.addEventListener("click", showPhotos);
+
+function showPhotos(event) {
+  let target = event.target.dataset.tab;
+
+  if (event.target.tagName !== "LI") return false;
+
+  activeCards.forEach((item) => {
+    if (item.classList.contains(target) || target === "all") {
+      item.style.display = "block";
     } else {
-        item.style.display = "none"
+      item.style.display = "none";
     }
-    
-})
+  });
+}
 
-}   
+const button = document.querySelector(".work__btn");
 
+button.addEventListener("click", (e) => {
+  e.preventDefault();
 
-const button = document.querySelector(".work__btn")
-
-button.addEventListener("click", e =>{
-    e.preventDefault();
-
-    unActiveCards.forEach(card=>{
-    card.style.display = "block"
-})
-    button.style.display = "none"
-})
-
-
+  unActiveCards.forEach((card) => {
+    card.style.display = "block";
+  });
+  button.style.display = "none";
+});
